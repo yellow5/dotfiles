b8e8948b10c9ae2431601e639c9ccff8cd85bf38
Cameron Dykes <yellow5c@gmail.com>
2011-05-14 20:06:24 -0500

    Add vim-ruby to vim bundles

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -36,6 +36,7 @@ get_bundle tpope vim-speeddating
 get_bundle tpope vim-surround
 get_bundle tpope vim-unimpaired
 get_bundle tpope vim-vividchalk
+get_bundle vim-ruby vim-ruby
 get_bundle wgibbs vim-irblack
 get_bundle scrooloose nerdtree
 get_bundle altercation vim-colors-solarized
